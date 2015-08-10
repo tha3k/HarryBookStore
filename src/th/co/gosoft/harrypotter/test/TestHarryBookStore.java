70000bdaf100774d24227a840ee95b574bf93b5f
tha3k <thapakornsah@GSSD_THAPAKORNS.7eleven.cp.co.th>
2015-08-10 14:43:30 +0700

    refactor    - rename method buy => addToCart    - change test name

--- src/th/co/gosoft/harrypotter/test/TestHarryBookStore.java
+++ src/th/co/gosoft/harrypotter/test/TestHarryBookStore.java
@@ -2,7 +2,6 @@ package th.co.gosoft.harrypotter.test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import th.co.gosoft.harrypotter.Book;
@@ -13,89 +12,89 @@ public class TestHarryBookStore {
 	HarryBookStore bookStore = new HarryBookStore();
 	
 	@Test 
-	public void buy_0_book_total_price_0() {
+	public void no_book_total_price_should_be_0() {
 		assertEquals(0.0, bookStore.getTotalPrice(), 0);
 	}
 
 	@Test
 	public void buy_1_1st_book_total_price_8EUR() {
-		bookStore.buy(new Book("1st"));
+		bookStore.addToCart(new Book("1st"));
 		assertEquals(8, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test 
 	public void buy_1_1st_book_and_1_2nd_book_should_have_2_book_type() {
-		bookStore.buy(new Book("1st"));
-		bookStore.buy(new Book("2nd"));
+		bookStore.addToCart(new Book("1st"));
+		bookStore.addToCart(new Book("2nd"));
 		assertEquals(2, bookStore.getShopingCart().size());
 	}
 
 	@Test
 	public void buy_1_1st_book_and_1_2nd_book_should_discount_5_percent() {
-		bookStore.buy(new Book("1st"));
-		bookStore.buy(new Book("2nd"));
+		bookStore.addToCart(new Book("1st"));
+		bookStore.addToCart(new Book("2nd"));
 		assertEquals(15.2, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test 
 	public void buy_1_1st_book_and_1_2nd_book_and_1_3rd_book_should_discount_10_percent() {
-		bookStore.buy(new Book("1st"));
-		bookStore.buy(new Book("2nd"));
-		bookStore.buy(new Book("3rd"));
+		bookStore.addToCart(new Book("1st"));
+		bookStore.addToCart(new Book("2nd"));
+		bookStore.addToCart(new Book("3rd"));
 		assertEquals(21.6, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test 
 	public void buy_1_1st_book_and_1_2nd_book_and_1_3rd_book_and_1_4th_book_should_discount_20_percent() {
-		bookStore.buy(new Book("1st"));
-		bookStore.buy(new Book("2nd"));
-		bookStore.buy(new Book("3rd"));
-		bookStore.buy(new Book("4th"));
+		bookStore.addToCart(new Book("1st"));
+		bookStore.addToCart(new Book("2nd"));
+		bookStore.addToCart(new Book("3rd"));
+		bookStore.addToCart(new Book("4th"));
 		assertEquals(25.6, bookStore.getTotalPrice(), 0);
 	}
 
 	@Test 
 	public void buy_1_1st_book_and_1_2nd_book_and_1_3rd_book_and_1_4th_book_and_1_5th_book_should_discount_25_percent() {
-		bookStore.buy(new Book("1st"));
-		bookStore.buy(new Book("2nd"));
-		bookStore.buy(new Book("3rd"));
-		bookStore.buy(new Book("4th"));
-		bookStore.buy(new Book("5th"));
+		bookStore.addToCart(new Book("1st"));
+		bookStore.addToCart(new Book("2nd"));
+		bookStore.addToCart(new Book("3rd"));
+		bookStore.addToCart(new Book("4th"));
+		bookStore.addToCart(new Book("5th"));
 		assertEquals(30, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test 
 	public void buy_2_1st_book_and_1_2nd_book() {
-		bookStore.buy(new Book("1st"), 2);
-		bookStore.buy(new Book("2nd"));
+		bookStore.addToCart(new Book("1st"), 2);
+		bookStore.addToCart(new Book("2nd"));
 		assertEquals(23.2, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test 
 	public void buy_2_1st_book_and_2_2nd_book() {
-		bookStore.buy(new Book("1st"), 2);
-		bookStore.buy(new Book("2nd"), 2);
+		bookStore.addToCart(new Book("1st"), 2);
+		bookStore.addToCart(new Book("2nd"), 2);
 		assertEquals(30.4, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test
 	public void buy_2_1st_book_and_2_2nd_book_and_2_3rd_book_and_1_4th_book_and_1_5th_book_should_discount_with_no_optimize() {
-		bookStore.buy(new Book("1st"), 2);
-		bookStore.buy(new Book("2nd"), 2);
-		bookStore.buy(new Book("3rd"), 2);
-		bookStore.buy(new Book("4th"));
-		bookStore.buy(new Book("5th"));
+		bookStore.addToCart(new Book("1st"), 2);
+		bookStore.addToCart(new Book("2nd"), 2);
+		bookStore.addToCart(new Book("3rd"), 2);
+		bookStore.addToCart(new Book("4th"));
+		bookStore.addToCart(new Book("5th"));
 		assertEquals(51.6, bookStore.getTotalPrice(), 0);
 	}
 	
 	@Test
 	public void buy_2_1st_book_and_2_2nd_book_and_2_3rd_book_and_1_4th_book_and_1_5th_book_should_discount_with_optimize() {
 		bookStore.setOptimized(true);
-		bookStore.buy(new Book("1st"), 2);
-		bookStore.buy(new Book("2nd"), 2);
-		bookStore.buy(new Book("3rd"), 2);
-		bookStore.buy(new Book("4th"));
-		bookStore.buy(new Book("5th"));
+		bookStore.addToCart(new Book("1st"), 2);
+		bookStore.addToCart(new Book("2nd"), 2);
+		bookStore.addToCart(new Book("3rd"), 2);
+		bookStore.addToCart(new Book("4th"));
+		bookStore.addToCart(new Book("5th"));
 		assertEquals(51.2, bookStore.getTotalPrice(), 0);
 	}
 	

7da50ad1abd072ea056db065e9f7e64e316f2ae0
tha3k <thapakornsah@GSSD_THAPAKORNS.7eleven.cp.co.th>
2015-08-10 11:36:30 +0700

    calculate with discount

--- src/th/co/gosoft/harrypotter/HarryBookStore.java
+++ src/th/co/gosoft/harrypotter/HarryBookStore.java
@@ -7,8 +7,10 @@ public class HarryBookStore {
 	private Map<String, Integer> shopingCart = new HashMap<String, Integer>();
 
 	public double getTotalPrice() {
-		double totalPrice = shopingCart.size()*8;
-		return totalPrice;
+		int bookTypeCount = shopingCart.size();
+		double totalPrice = bookTypeCount*8;
+		double discountPrice = totalPrice*getDiscountPercent(bookTypeCount);
+		return totalPrice-discountPrice;
 	}
 
 	public void buy(Book book) {
@@ -17,5 +19,23 @@ public class HarryBookStore {
 			bookCount = shopingCart.get(book.getName())+1;
 		shopingCart.put(book.getName(), bookCount);
 	}
+
+	public Map<String, Integer> getShopingCart() {
+		return shopingCart;
+	}
+	
+	private double getDiscountPercent(int bookTypeCount) {
+		if (bookTypeCount==2)
+			return 0.05;
+		else if (bookTypeCount==3)
+			return 0.10;
+		else if (bookTypeCount==4)
+			return 0.20;
+		else if (bookTypeCount==5)
+			return 0.25;
+		
+		return 0;
+	}
+	
 
 }
--- src/th/co/gosoft/harrypotter/TestHarryBookStore.java
+++ src/th/co/gosoft/harrypotter/TestHarryBookStore.java
@@ -20,5 +20,22 @@ public class TestHarryBookStore {
 		assertEquals(8, bookStore.getTotalPrice(), 0);
 	}
 	
+	@Test 
+	public void buy_1_1st_book_and_1_2nd_book_should_have_2_book_type() {
+		HarryBookStore bookStore = new HarryBookStore();
+		bookStore.buy(new Book("1st"));
+		bookStore.buy(new Book("2nd"));
+		assertEquals(2, bookStore.getShopingCart().size());
+	}
+
+	@Test
+	public void buy_1_1st_book_and_1_2nd_book_should_discount_5_percent() {
+		HarryBookStore bookStore = new HarryBookStore();
+		bookStore.buy(new Book("1st"));
+		bookStore.buy(new Book("2nd"));
+		assertEquals(15.2, bookStore.getTotalPrice(), 0);
+	}
+	
+	
 	
 }
